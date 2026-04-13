43b2b7dc6cecec9fc4dae6d48f0c80db238a3f74
myquicktime <1971182908@qq.com>
2026-04-13 13:54:36 +0800

    初始

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,79 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div v-for="(item, index) in booklist" :key="index" class="booklist" @click="todetail(item)">
+      {{item.title}}
+    </div>
+
+    <div>
+      {{title}}
+      <div v-for="(item, index) in detaillist" :key="index">
+{{item.read_seconds}}
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  import axios from 'axios';
+  export default {
+    name: 'HelloWorld',
+    props: {
+      msg: String,
+    },
+    data() {
+    return {
+      booklist: [] ,// 存储读取的JSON数据
+      recordlist:[],
+      title:'',
+      detaillist:[]
+    };
+  },
+    mounted() {
+      this.loadJsonData();
+    },
+    methods: {
+      async loadJsonData() {
+       
+         try {
+          // 请求public目录下的ziyuan.json（/代表public根目录）
+          const response = await axios.get('/book.json');
+          this.booklist = response.data; // 赋值到data中
+          console.log('读取成功：', this.booklist);
+        } catch (error) {
+          console.error('读取失败：', error);
+          this.jsonData = '读取失败，请检查文件路径';
+        }
+         try {
+          // 请求public目录下的ziyuan.json（/代表public根目录）
+          const response = await axios.get('/read_record.json');
+          this.recordlist = response.data; // 赋值到data中
+          console.log('读取成功：', this.recordlist);
+        } catch (error) {
+          console.error('读取失败：', error);
+          this.jsonData = '读取失败，请检查文件路径';
+        }
+      },
+      todetail(item){
+        this.title=item.title
+       this.detaillist= this.recordlist.filter((record)=>{
+        console.log(record.book_id)
+        console.log(item.id)
+          return record.book_id==item.id
+
+        })
+        console.log(this.detaillist)
+
+      }
+    }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  .booklist{
+    height: 60px;
+  margin: 5px;
+  background-color: bisque;
+  }
 </style>
